318b9385804bebc5805089e43191c53d058b7390
nantaswd <anantawijaya.swd@gmail.com>
2025-09-03 15:45:14 +0800

    Changes for testing only

--- jira_utility/jira_api_actions.py
+++ jira_utility/jira_api_actions.py
@@ -57,14 +57,14 @@ class JiraApiActions:
                 "description": str(fields_values['description']),
                 "summary": fields_values["summary"],
                 self.fields["lbu"] : fields_values["lbu"],
-                self.fields["project_name"] : fields_values["project_name"],
+                # self.fields["project_name"] : fields_values["project_name"],
                 self.fields["application_name"] : fields_values["application_name"],
                 self.fields["scan_report_link"] : fields_values["scan_report_link"],
                 self.fields["num_of_critical"] : fields_values["num_of_critical"],
                 self.fields["num_of_high"] : fields_values["num_of_high"],
                 self.fields["num_of_medium"] : fields_values["num_of_medium"],
                 self.fields["num_of_low"] : fields_values["num_of_low"],
-                # self.fields["scan_url"] : fields_values['scan_url'],
+                self.fields["scan_url"] : fields_values['scan_url'],
                 self.fields["tag"] : fields_values['tag'],
             }
         }
